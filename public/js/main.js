5807d268ca753a1313b2a06a667f5f21aaac5122
Mark Heramis <chumheramis@gmail.com>
2016-09-13 17:27:08 +0800

    public updates

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,17 @@
 $(document).on('ready', function(){
-        var socket = io('http://localhost:9090');
-        window.app = feathers()
-          .configure(feathers.socketio(socket))
-          .configure(feathers.hooks())
-          .configure(feathers.authentication({ storage: window.localStorage }));
-        app.authenticate({
-          'type': 'local',
-          'email': "paul@gmail.com",
-          'password': "admin123"
-        }).then(function(result){
-          console.log('Authenticated!', result);
-        }).catch(function(error){
-          console.error('Error authenticating!', error);
-          //alert('Error: ' + error.message);
-        });
-      });
+  var socket = io('http://localhost:9090');
+  window.app = feathers()
+    .configure(feathers.socketio(socket))
+    .configure(feathers.hooks())
+    .configure(feathers.authentication({ storage: window.localStorage }));
+  app.authenticate({
+    'type': 'local',
+    'email': "chumheramis@gmail.com",
+    'password': "admin123"
+  }).then(function(result){
+    console.log('Authenticated!', result);
+  }).catch(function(error){
+    console.error('Error authenticating!', error);
+    //alert('Error: ' + error.message);
+  });
+});
